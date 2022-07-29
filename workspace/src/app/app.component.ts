b345c6a68297faa013c0bd282339516c1f667015
Kadota <joaokadota@gmail.com>
2022-07-28 23:24:44 -0300

    modified:   workspace/angular.json 	modified:   workspace/package-lock.json 	modified:   workspace/package.json 	modified:   workspace/src/app/app.component.css 	modified:   workspace/src/app/app.component.html 	modified:   workspace/src/app/app.component.ts 	modified:   workspace/src/app/app.module.ts 	new file:   workspace/src/app/direct/marca-texto.directive.spec.ts 	new file:   workspace/src/app/direct/marca-texto.directive.ts

--- workspace/src/app/app.component.ts
+++ workspace/src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'workspace';
+export class AppComponent implements OnInit {
+  count = 0;
+  nome = 'João Paulo Kadota';
+  text = '';
+
+  pessoas = [
+    {
+      nome: "Ivonaldo",
+      sobrenome: "Soares"
+    },
+    {
+      nome: "João",
+      sobrenome: "kadota"
+    },
+    {
+      nome: "Natália",
+      sobrenome: "Silva"
+    },
+    {
+      nome: "Tony",
+      sobrenome: "Kadota"
+    }
+  ]
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    console.log(this.pessoas)
+      let interval = setInterval(() => {
+        this.count++;
+        if(this.count === 10) {
+          clearInterval(interval);
+        }
+      }, 1000)
+  }
+
+  clicou(nome: string): void{
+    console.log('Clicou em min', nome);
+  }
 }
